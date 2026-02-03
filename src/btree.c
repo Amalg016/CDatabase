7362bc3d4a9329d73a40cad479fe5d970f117b41
Amalg016 <amalgjoseph1@gmail.com>
2026-02-04 00:08:20 +0530

    indentations

--- src/btree.c
+++ src/btree.c
@@ -1,183 +1,184 @@
 #include "../include/btree.h"
 
 // Get node type
-NodeType get_node_type(void* node) {
-    uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
-    return (NodeType)value;
+NodeType get_node_type(void *node) {
+  uint8_t value = *((uint8_t *)(node + NODE_TYPE_OFFSET));
+  return (NodeType)value;
 }
 
-void set_node_type(void* node, NodeType type) {
-    uint8_t value = type;
-    *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+void set_node_type(void *node, NodeType type) {
+  uint8_t value = type;
+  *((uint8_t *)(node + NODE_TYPE_OFFSET)) = value;
 }
 
 // Root node operations
-bool is_node_root(void* node) {
-    uint8_t value = *((uint8_t*)(node + IS_ROOT_OFFSET));
-    return (bool)value;
+bool is_node_root(void *node) {
+  uint8_t value = *((uint8_t *)(node + IS_ROOT_OFFSET));
+  return (bool)value;
 }
 
-void set_node_root(void* node, bool is_root) {
-    uint8_t value = is_root;
-    *((uint8_t*)(node + IS_ROOT_OFFSET)) = value;
+void set_node_root(void *node, bool is_root) {
+  uint8_t value = is_root;
+  *((uint8_t *)(node + IS_ROOT_OFFSET)) = value;
 }
 
 // Parent pointer operations
-uint32_t* node_parent(void* node) {
-    return (uint32_t*)(node + PARENT_POINTER_OFFSET);
+uint32_t *node_parent(void *node) {
+  return (uint32_t *)(node + PARENT_POINTER_OFFSET);
 }
 
 // Leaf node operations
-uint32_t* leaf_node_num_cells(void* node) {
-    return (uint32_t*)(node + LEAF_NODE_NUM_CELLS_OFFSET);
+uint32_t *leaf_node_num_cells(void *node) {
+  return (uint32_t *)(node + LEAF_NODE_NUM_CELLS_OFFSET);
 }
 
-uint32_t* leaf_node_next_leaf(void* node) {
-    return (uint32_t*)(node + LEAF_NODE_NEXT_LEAF_OFFSET);
+uint32_t *leaf_node_next_leaf(void *node) {
+  return (uint32_t *)(node + LEAF_NODE_NEXT_LEAF_OFFSET);
 }
 
-void* leaf_node_cell(void* node, uint32_t cell_num) {
-    return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+void *leaf_node_cell(void *node, uint32_t cell_num) {
+  return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
 }
 
-uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
-    return (uint32_t*)leaf_node_cell(node, cell_num);
+uint32_t *leaf_node_key(void *node, uint32_t cell_num) {
+  return (uint32_t *)leaf_node_cell(node, cell_num);
 }
 
-void* leaf_node_value(void* node, uint32_t cell_num) {
-    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+void *leaf_node_value(void *node, uint32_t cell_num) {
+  return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
-void initialize_leaf_node(void* node) {
-    set_node_type(node, NODE_LEAF);
-    set_node_root(node, false);
-    *leaf_node_num_cells(node) = 0;
-    *leaf_node_next_leaf(node) = 0; // 0 represents no sibling
+void initialize_leaf_node(void *node) {
+  set_node_type(node, NODE_LEAF);
+  set_node_root(node, false);
+  *leaf_node_num_cells(node) = 0;
+  *leaf_node_next_leaf(node) = 0; // 0 represents no sibling
 }
 
 // Internal node operations
-uint32_t* internal_node_num_keys(void* node) {
-    return (uint32_t*)(node + INTERNAL_NODE_NUM_KEYS_OFFSET);
+uint32_t *internal_node_num_keys(void *node) {
+  return (uint32_t *)(node + INTERNAL_NODE_NUM_KEYS_OFFSET);
 }
 
-uint32_t* internal_node_right_child(void* node) {
-    return (uint32_t*)(node + INTERNAL_NODE_RIGHT_CHILD_OFFSET);
+uint32_t *internal_node_right_child(void *node) {
+  return (uint32_t *)(node + INTERNAL_NODE_RIGHT_CHILD_OFFSET);
 }
 
-uint32_t* internal_node_cell(void* node, uint32_t cell_num) {
-    return (uint32_t*)(node + INTERNAL_NODE_HEADER_SIZE + cell_num * INTERNAL_NODE_CELL_SIZE);
+uint32_t *internal_node_cell(void *node, uint32_t cell_num) {
+  return (uint32_t *)(node + INTERNAL_NODE_HEADER_SIZE +
+                      cell_num * INTERNAL_NODE_CELL_SIZE);
 }
 
-uint32_t* internal_node_child(void* node, uint32_t child_num) {
-    uint32_t num_keys = *internal_node_num_keys(node);
-    if (child_num > num_keys) {
-        printf("Tried to access child_num %d > num_keys %d\n", child_num, num_keys);
-        exit(EXIT_FAILURE);
-    } else if (child_num == num_keys) {
-        return internal_node_right_child(node);
-    } else {
-        return internal_node_cell(node, child_num);
-    }
+uint32_t *internal_node_child(void *node, uint32_t child_num) {
+  uint32_t num_keys = *internal_node_num_keys(node);
+  if (child_num > num_keys) {
+    printf("Tried to access child_num %d > num_keys %d\n", child_num, num_keys);
+    exit(EXIT_FAILURE);
+  } else if (child_num == num_keys) {
+    return internal_node_right_child(node);
+  } else {
+    return internal_node_cell(node, child_num);
+  }
 }
 
-uint32_t* internal_node_key(void* node, uint32_t key_num) {
-    return (uint32_t*)((void*)internal_node_cell(node, key_num) + INTERNAL_NODE_CHILD_SIZE);
+uint32_t *internal_node_key(void *node, uint32_t key_num) {
+  return (uint32_t *)((void *)internal_node_cell(node, key_num) +
+                      INTERNAL_NODE_CHILD_SIZE);
 }
 
-void initialize_internal_node(void* node) {
-    set_node_type(node, NODE_INTERNAL);
-    set_node_root(node, false);
-    *internal_node_num_keys(node) = 0;
+void initialize_internal_node(void *node) {
+  set_node_type(node, NODE_INTERNAL);
+  set_node_root(node, false);
+  *internal_node_num_keys(node) = 0;
 }
 
 // Find the position where a key should be inserted in a leaf node
-uint32_t leaf_node_find(void* node, uint32_t key) {
-    uint32_t num_cells = *leaf_node_num_cells(node);
-    uint32_t min_index = 0;
-    uint32_t one_past_max_index = num_cells;
+uint32_t leaf_node_find(void *node, uint32_t key) {
+  uint32_t num_cells = *leaf_node_num_cells(node);
+  uint32_t min_index = 0;
+  uint32_t one_past_max_index = num_cells;
 
-    while (one_past_max_index != min_index) {
-        uint32_t index = (min_index + one_past_max_index) / 2;
-        uint32_t key_at_index = *leaf_node_key(node, index);
-        if (key == key_at_index) {
-            return index;
-        }
-        if (key < key_at_index) {
-            one_past_max_index = index;
-        } else {
-            min_index = index + 1;
-        }
+  while (one_past_max_index != min_index) {
+    uint32_t index = (min_index + one_past_max_index) / 2;
+    uint32_t key_at_index = *leaf_node_key(node, index);
+    if (key == key_at_index) {
+      return index;
     }
+    if (key < key_at_index) {
+      one_past_max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
 
-    return min_index;
+  return min_index;
 }
 
 // Find child in internal node
-uint32_t internal_node_find_child(void* node, uint32_t key) {
-    uint32_t num_keys = *internal_node_num_keys(node);
+uint32_t internal_node_find_child(void *node, uint32_t key) {
+  uint32_t num_keys = *internal_node_num_keys(node);
 
-    uint32_t min_index = 0;
-    uint32_t max_index = num_keys;
+  uint32_t min_index = 0;
+  uint32_t max_index = num_keys;
 
-    while (min_index != max_index) {
-        uint32_t index = (min_index + max_index) / 2;
-        uint32_t key_to_right = *internal_node_key(node, index);
-        if (key_to_right >= key) {
-            max_index = index;
-        } else {
-            min_index = index + 1;
-        }
+  while (min_index != max_index) {
+    uint32_t index = (min_index + max_index) / 2;
+    uint32_t key_to_right = *internal_node_key(node, index);
+    if (key_to_right >= key) {
+      max_index = index;
+    } else {
+      min_index = index + 1;
     }
+  }
 
-    return min_index;
+  return min_index;
 }
 
 // Get the maximum key in a node (recursive)
-uint32_t get_node_max_key(Pager* pager, void* node) {
-    if (get_node_type(node) == NODE_LEAF) {
-        uint32_t num_cells = *leaf_node_num_cells(node);
-        return *leaf_node_key(node, num_cells - 1);
-    }
-    void* right_child = pager_get_page(pager, *internal_node_right_child(node));
-    return get_node_max_key(pager, right_child);
+uint32_t get_node_max_key(Pager *pager, void *node) {
+  if (get_node_type(node) == NODE_LEAF) {
+    uint32_t num_cells = *leaf_node_num_cells(node);
+    return *leaf_node_key(node, num_cells - 1);
+  }
+  void *right_child = pager_get_page(pager, *internal_node_right_child(node));
+  return get_node_max_key(pager, right_child);
 }
 
-void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level) {
-    void* node = pager_get_page(pager, page_num);
-    uint32_t num_keys, child;
+void print_tree(Pager *pager, uint32_t page_num, uint32_t indentation_level) {
+  void *node = pager_get_page(pager, page_num);
+  uint32_t num_keys, child;
 
-    switch (get_node_type(node)) {
-        case NODE_LEAF:
-            num_keys = *leaf_node_num_cells(node);
-            for (uint32_t i = 0; i < indentation_level; i++) {
-                printf("  ");
-            }
-            printf("- leaf (size %d)\n", num_keys);
-            for (uint32_t i = 0; i < num_keys; i++) {
-                for (uint32_t j = 0; j < indentation_level + 1; j++) {
-                    printf("  ");
-                }
-                printf("- %d\n", *leaf_node_key(node, i));
-            }
-            break;
-        case NODE_INTERNAL:
-            num_keys = *internal_node_num_keys(node);
-            for (uint32_t i = 0; i < indentation_level; i++) {
-                printf("  ");
-            }
-            printf("- internal (size %d)\n", num_keys);
-            for (uint32_t i = 0; i < num_keys; i++) {
-                child = *internal_node_child(node, i);
-                print_tree(pager, child, indentation_level + 1);
-
-                for (uint32_t j = 0; j < indentation_level + 1; j++) {
-                    printf("  ");
-                }
-                printf("- key %d\n", *internal_node_key(node, i));
-            }
-            child = *internal_node_right_child(node);
-            print_tree(pager, child, indentation_level + 1);
-            break;
+  switch (get_node_type(node)) {
+  case NODE_LEAF:
+    num_keys = *leaf_node_num_cells(node);
+    for (uint32_t i = 0; i < indentation_level; i++) {
+      printf("  ");
     }
-}
+    printf("- leaf (size %d)\n", num_keys);
+    for (uint32_t i = 0; i < num_keys; i++) {
+      for (uint32_t j = 0; j < indentation_level + 1; j++) {
+        printf("  ");
+      }
+      printf("- %d\n", *leaf_node_key(node, i));
+    }
+    break;
+  case NODE_INTERNAL:
+    num_keys = *internal_node_num_keys(node);
+    for (uint32_t i = 0; i < indentation_level; i++) {
+      printf("  ");
+    }
+    printf("- internal (size %d)\n", num_keys);
+    for (uint32_t i = 0; i < num_keys; i++) {
+      child = *internal_node_child(node, i);
+      print_tree(pager, child, indentation_level + 1);
 
+      for (uint32_t j = 0; j < indentation_level + 1; j++) {
+        printf("  ");
+      }
+      printf("- key %d\n", *internal_node_key(node, i));
+    }
+    child = *internal_node_right_child(node);
+    print_tree(pager, child, indentation_level + 1);
+    break;
+  }
+}
--- src/table.c
+++ src/table.c
@@ -1,128 +1,126 @@
 #include "../include/table.h"
 #include "../include/btree.h"
 
+Table *table_create(const char *filename, Schema *schema) {
+  Pager *pager = pager_open(filename);
 
-Table* table_create(const char* filename, Schema* schema) {
-    Pager* pager = pager_open(filename);
+  Table *table = (Table *)malloc(sizeof(Table));
+  table->pager = pager;
+  table->schema = schema;
 
-    Table* table = (Table*) malloc(sizeof(Table));
-    table->pager = pager;
-    table->schema = schema;
+  if (pager->num_pages == 0) {
+    // New database file. Initialize page 0 as leaf node.
+    void *root_node = pager_get_page(pager, 0);
+    initialize_leaf_node(root_node);
+    set_node_root(root_node, true);
+  }
 
-    if (pager->num_pages == 0) {
-        // New database file. Initialize page 0 as leaf node.
-        void* root_node = pager_get_page(pager, 0);
-        initialize_leaf_node(root_node);
-        set_node_root(root_node, true);
-    }
+  table->root_page_num = 0;
 
-    table->root_page_num = 0;
-
-    return table;
+  return table;
 }
 
 // Close table and free resources
-void table_close(Table* table) {
-    pager_close(table->pager);
-    if (table->schema->columns) {
-        free(table->schema->columns);
-    }
-    free(table->schema);
-    free(table);
+void table_close(Table *table) {
+  pager_close(table->pager);
+  if (table->schema->columns) {
+    free(table->schema->columns);
+  }
+  free(table->schema);
+  free(table);
 }
 
 // Serialize a row into a buffer based on schema
-void serialize_row(Schema* schema, void** values, void* destination) {
-    uint32_t offset = 0;
-    for (uint32_t i = 0; i < schema->num_columns; i++) {
-        Column* col = &schema->columns[i];
-        if (col->type == COL_TYPE_INT) {
-            memcpy(destination + offset, values[i], sizeof(int32_t));
-            offset += sizeof(int32_t);
-        } else if (col->type == COL_TYPE_TEXT) {
-            memcpy(destination + offset, values[i], col->size);
-            offset += col->size;
-        }
+void serialize_row(Schema *schema, void **values, void *destination) {
+  uint32_t offset = 0;
+  for (uint32_t i = 0; i < schema->num_columns; i++) {
+    Column *col = &schema->columns[i];
+    if (col->type == COL_TYPE_INT) {
+      memcpy(destination + offset, values[i], sizeof(int32_t));
+      offset += sizeof(int32_t);
+    } else if (col->type == COL_TYPE_TEXT) {
+      memcpy(destination + offset, values[i], col->size);
+      offset += col->size;
     }
+  }
 }
 
 // Deserialize a row from buffer based on schema
-void** deserialize_row(Schema* schema, void* source) {
-    void** values = malloc(sizeof(void*) * schema->num_columns);
-    uint32_t offset = 0;
+void **deserialize_row(Schema *schema, void *source) {
+  void **values = malloc(sizeof(void *) * schema->num_columns);
+  uint32_t offset = 0;
 
-    for (uint32_t i = 0; i < schema->num_columns; i++) {
-        Column* col = &schema->columns[i];
-        if (col->type == COL_TYPE_INT) {
-            values[i] = malloc(sizeof(int32_t));
-            memcpy(values[i], source + offset, sizeof(int32_t));
-            offset += sizeof(int32_t);
-        } else if (col->type == COL_TYPE_TEXT) {
-            values[i] = malloc(col->size);
-            memcpy(values[i], source + offset, col->size);
-            offset += col->size;
-        }
+  for (uint32_t i = 0; i < schema->num_columns; i++) {
+    Column *col = &schema->columns[i];
+    if (col->type == COL_TYPE_INT) {
+      values[i] = malloc(sizeof(int32_t));
+      memcpy(values[i], source + offset, sizeof(int32_t));
+      offset += sizeof(int32_t);
+    } else if (col->type == COL_TYPE_TEXT) {
+      values[i] = malloc(col->size);
+      memcpy(values[i], source + offset, col->size);
+      offset += col->size;
     }
+  }
 
-    return values;
+  return values;
 }
 
 // Print a row
-void print_row(Schema* schema, void** values) {
-    for (uint32_t i = 0; i < schema->num_columns; i++) {
-        Column* col = &schema->columns[i];
-        printf("%s: ", col->name);
-        if (col->type == COL_TYPE_INT) {
-            printf("%d", *(int32_t*)values[i]);
-        } else if (col->type == COL_TYPE_TEXT) {
-            printf("%s", (char*)values[i]);
-        }
-        if (i < schema->num_columns - 1) {
-            printf(", ");
-        }
+void print_row(Schema *schema, void **values) {
+  for (uint32_t i = 0; i < schema->num_columns; i++) {
+    Column *col = &schema->columns[i];
+    printf("%s: ", col->name);
+    if (col->type == COL_TYPE_INT) {
+      printf("%d", *(int32_t *)values[i]);
+    } else if (col->type == COL_TYPE_TEXT) {
+      printf("%s", (char *)values[i]);
     }
-    printf("\n");
+    if (i < schema->num_columns - 1) {
+      printf(", ");
+    }
+  }
+  printf("\n");
 }
 
 // Calculate row size from schema
-uint32_t calculate_row_size(Schema* schema) {
-    uint32_t size = 0;
-    for (uint32_t i = 0; i < schema->num_columns; i++) {
-        Column* col = &schema->columns[i];
-        if (col->type == COL_TYPE_INT) {
-            size += sizeof(int32_t);
-        } else if (col->type == COL_TYPE_TEXT) {
-            size += col->size;
-        }
+uint32_t calculate_row_size(Schema *schema) {
+  uint32_t size = 0;
+  for (uint32_t i = 0; i < schema->num_columns; i++) {
+    Column *col = &schema->columns[i];
+    if (col->type == COL_TYPE_INT) {
+      size += sizeof(int32_t);
+    } else if (col->type == COL_TYPE_TEXT) {
+      size += col->size;
     }
-    return size;
+  }
+  return size;
 }
 
 // Create a schema
-Schema* schema_create(const char* table_name, uint32_t num_columns) {
-    Schema* schema = (Schema*)malloc(sizeof(Schema));
-    strncpy(schema->name, table_name, 31);
-    schema->name[31] = '\0';
-    schema->num_columns = num_columns;
-    schema->columns = (Column*)malloc(sizeof(Column) * num_columns);
-    schema->row_size = 0;
-    return schema;
+Schema *schema_create(const char *table_name, uint32_t num_columns) {
+  Schema *schema = (Schema *)malloc(sizeof(Schema));
+  strncpy(schema->name, table_name, 31);
+  schema->name[31] = '\0';
+  schema->num_columns = num_columns;
+  schema->columns = (Column *)malloc(sizeof(Column) * num_columns);
+  schema->row_size = 0;
+  return schema;
 }
 
 // Add a column to schema
-void schema_add_column(Schema* schema, uint32_t index, const char* name, 
+void schema_add_column(Schema *schema, uint32_t index, const char *name,
                        ColumnType type, uint32_t size) {
-    if (index >= schema->num_columns) {
-        printf("Column index out of bounds\n");
-        return;
-    }
+  if (index >= schema->num_columns) {
+    printf("Column index out of bounds\n");
+    return;
+  }
 
-    Column* col = &schema->columns[index];
-    strncpy(col->name, name, 31);
-    col->name[31] = '\0';
-    col->type = type;
-    col->size = (type == COL_TYPE_TEXT) ? size : sizeof(int32_t);
+  Column *col = &schema->columns[index];
+  strncpy(col->name, name, 31);
+  col->name[31] = '\0';
+  col->type = type;
+  col->size = (type == COL_TYPE_TEXT) ? size : sizeof(int32_t);
 
-    schema->row_size = calculate_row_size(schema);
+  schema->row_size = calculate_row_size(schema);
 }
-
